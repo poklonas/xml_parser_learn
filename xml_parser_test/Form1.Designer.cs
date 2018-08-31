564c45db4b303d9e751853566e343f570eefd253
sarik <a5810164@gmail.com>
2018-08-31 16:11:46 +0700

    read and save changing fille but schema not work

--- xml_parser_test/Form1.Designer.cs
+++ xml_parser_test/Form1.Designer.cs
@@ -31,9 +31,21 @@
             this.helpProvider1 = new System.Windows.Forms.HelpProvider();
             this.openFiles = new System.Windows.Forms.Button();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.output_box = new System.Windows.Forms.TextBox();
             this.pathLabel = new System.Windows.Forms.Label();
             this.selectFile = new System.Windows.Forms.OpenFileDialog();
-            this.output_box = new System.Windows.Forms.TextBox();
+            this.input_name = new System.Windows.Forms.TextBox();
+            this.input_descrip = new System.Windows.Forms.TextBox();
+            this.input_price = new System.Windows.Forms.TextBox();
+            this.add = new System.Windows.Forms.Button();
+            this.input_cal = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.save = new System.Windows.Forms.Button();
+            this.only_name = new System.Windows.Forms.Button();
+            this.allSchema = new System.Windows.Forms.Button();
             this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -53,6 +65,7 @@
             // 
             // groupBox1
             // 
+            this.groupBox1.Controls.Add(this.pathLabel);
             this.groupBox1.Controls.Add(this.output_box);
             this.groupBox1.ForeColor = System.Drawing.Color.Navy;
             this.groupBox1.Location = new System.Drawing.Point(36, 59);
@@ -61,20 +74,6 @@
             this.groupBox1.TabIndex = 0;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "output";
-            // 
-            // pathLabel
-            // 
-            this.pathLabel.AutoSize = true;
-            this.pathLabel.Location = new System.Drawing.Point(299, 22);
-            this.pathLabel.Name = "pathLabel";
-            this.pathLabel.Size = new System.Drawing.Size(29, 13);
-            this.pathLabel.TabIndex = 2;
-            this.pathLabel.Text = "Path";
-            // 
-            // selectFile
-            // 
-            this.selectFile.FileName = "selectFile";
-            this.selectFile.Filter = "XML|*xml";
             // 
             // output_box
             // 
@@ -86,13 +85,148 @@
             this.output_box.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
             this.output_box.Size = new System.Drawing.Size(683, 331);
             this.output_box.TabIndex = 0;
+            this.output_box.TextChanged += new System.EventHandler(this.output_box_TextChanged);
+            // 
+            // pathLabel
+            // 
+            this.pathLabel.AutoSize = true;
+            this.pathLabel.Location = new System.Drawing.Point(13, 14);
+            this.pathLabel.Name = "pathLabel";
+            this.pathLabel.Size = new System.Drawing.Size(29, 13);
+            this.pathLabel.TabIndex = 2;
+            this.pathLabel.Text = "Path";
+            // 
+            // selectFile
+            // 
+            this.selectFile.FileName = "selectFile";
+            this.selectFile.Filter = "XML|*xml";
+            // 
+            // input_name
+            // 
+            this.input_name.Location = new System.Drawing.Point(137, 445);
+            this.input_name.Name = "input_name";
+            this.input_name.Size = new System.Drawing.Size(233, 20);
+            this.input_name.TabIndex = 2;
+            this.input_name.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // input_descrip
+            // 
+            this.input_descrip.Location = new System.Drawing.Point(137, 468);
+            this.input_descrip.Name = "input_descrip";
+            this.input_descrip.Size = new System.Drawing.Size(233, 20);
+            this.input_descrip.TabIndex = 3;
+            // 
+            // input_price
+            // 
+            this.input_price.Location = new System.Drawing.Point(425, 445);
+            this.input_price.Name = "input_price";
+            this.input_price.Size = new System.Drawing.Size(87, 20);
+            this.input_price.TabIndex = 4;
+            // 
+            // add
+            // 
+            this.add.BackColor = System.Drawing.Color.Coral;
+            this.add.Location = new System.Drawing.Point(518, 442);
+            this.add.Name = "add";
+            this.add.Size = new System.Drawing.Size(237, 45);
+            this.add.TabIndex = 5;
+            this.add.Text = "ADD";
+            this.add.UseVisualStyleBackColor = false;
+            this.add.Click += new System.EventHandler(this.add_Click);
+            // 
+            // input_cal
+            // 
+            this.input_cal.Location = new System.Drawing.Point(425, 471);
+            this.input_cal.Name = "input_cal";
+            this.input_cal.Size = new System.Drawing.Size(87, 20);
+            this.input_cal.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(389, 445);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(30, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "price";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(376, 471);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(43, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "calories";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(98, 448);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(33, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "name";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(73, 468);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(58, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "description";
+            // 
+            // save
+            // 
+            this.save.BackColor = System.Drawing.Color.Pink;
+            this.save.Location = new System.Drawing.Point(606, 12);
+            this.save.Name = "save";
+            this.save.Size = new System.Drawing.Size(149, 33);
+            this.save.TabIndex = 10;
+            this.save.Text = "Save";
+            this.save.UseVisualStyleBackColor = false;
+            this.save.Click += new System.EventHandler(this.save_Click);
+            // 
+            // only_name
+            // 
+            this.only_name.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.only_name.Location = new System.Drawing.Point(265, 12);
+            this.only_name.Name = "only_name";
+            this.only_name.Size = new System.Drawing.Size(134, 33);
+            this.only_name.TabIndex = 11;
+            this.only_name.Text = "Only Food Name";
+            this.only_name.UseVisualStyleBackColor = false;
+            this.only_name.Click += new System.EventHandler(this.only_name_Click);
+            // 
+            // allSchema
+            // 
+            this.allSchema.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.allSchema.Location = new System.Drawing.Point(405, 12);
+            this.allSchema.Name = "allSchema";
+            this.allSchema.Size = new System.Drawing.Size(134, 33);
+            this.allSchema.TabIndex = 12;
+            this.allSchema.Text = "show all";
+            this.allSchema.UseVisualStyleBackColor = false;
+            this.allSchema.Click += new System.EventHandler(this.allSchema_Click);
             // 
             // XML_PARSER
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.pathLabel);
+            this.ClientSize = new System.Drawing.Size(794, 499);
+            this.Controls.Add(this.allSchema);
+            this.Controls.Add(this.only_name);
+            this.Controls.Add(this.save);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.input_cal);
+            this.Controls.Add(this.add);
+            this.Controls.Add(this.input_price);
+            this.Controls.Add(this.input_descrip);
+            this.Controls.Add(this.input_name);
             this.Controls.Add(this.openFiles);
             this.Controls.Add(this.groupBox1);
             this.Name = "XML_PARSER";
@@ -112,6 +246,18 @@
         private System.Windows.Forms.Label pathLabel;
         private System.Windows.Forms.OpenFileDialog selectFile;
         private System.Windows.Forms.TextBox output_box;
+        private System.Windows.Forms.TextBox input_name;
+        private System.Windows.Forms.TextBox input_descrip;
+        private System.Windows.Forms.TextBox input_price;
+        private System.Windows.Forms.Button add;
+        private System.Windows.Forms.TextBox input_cal;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button save;
+        private System.Windows.Forms.Button only_name;
+        private System.Windows.Forms.Button allSchema;
     }
 }
 
--- xml_parser_test/Form1.cs
+++ xml_parser_test/Form1.cs
@@ -8,15 +8,28 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+using System.Xml;
+using System.Xml.Schema;
 using System.Xml.Linq;
+using System.IO;
 
 namespace xml_parser_test
 {
+
     public partial class XML_PARSER : Form
     {
+        private XElement xmlFileLinq;
+        private string path;
+        private string maskUp;
+        private int mode = 0;
+        // using System.Xml.Schema;
+        private XmlSchemaSet schemas;
+
         public XML_PARSER()
         {
             InitializeComponent();
+            setMaskUp();
+            setSchema();
         }
 
         private void openFile_FileOk(object sender, CancelEventArgs e)
@@ -26,18 +39,110 @@ namespace xml_parser_test
 
         private void openFiles_Click(object sender, EventArgs e)
         {
+            this.output_box.Text = "";
             this.selectFile.ShowDialog();
-            string path = this.selectFile.FileName;
-            this.pathLabel.Text = path;
-            XElement xmlFile = XElement.Load(path);
-            if(path == "")
+            this.path = this.selectFile.FileName;
+            this.pathLabel.Text = this.path;
+            XmlDocument xmlFile = new XmlDocument();
+            xmlFile.Load(this.path);
+            this.xmlFileLinq = XElement.Load(this.path);
+            this.output_box.Text += this.xmlFileLinq;
+            this.mode = 1;
+        }
+
+        private void save_Click(object sender, EventArgs e)
+        {
+            XDocument check = new XDocument(this.xmlFileLinq);
+            bool checked_doc = false;
+            check.Validate(this.schemas, (o, er) => { this.output_box.Text += er.Message;  checked_doc = true;});
+            if (!checked_doc)
             {
-                return;
+                this.xmlFileLinq.Save(this.path, SaveOptions.None);
             }
-            this.output_box.Text += xmlFile;
-            //foreach( XElement ele in xmlFile.DescendantsAndSelf()){
-            //    this.output_box.Text += ele;
-            //}
+            else{
+                MessageBox.Show("คุณได้ เพิ่ม ข้อมูลที่ผิดพลาด ทำให้เซฟไม่ได้");
+            }
+        }
+
+        private void add_Click(object sender, EventArgs e)
+        {
+            XElement newElement = new XElement("food",
+                                           new XElement("name", this.input_name.Text),
+                                           new XElement("price", this.input_price.Text),
+                                           new XElement("description", this.input_descrip.Text),
+                                           new XElement("calories", this.input_cal.Text));
+            this.xmlFileLinq.LastNode.AddAfterSelf(newElement);
+            if (this.mode == 1)
+            {
+                this.output_box.Text += newElement;
+            }
+            else if(this.mode == 2)
+            {
+                this.output_box.Text = "";
+                IEnumerable<XElement> foods = from food in this.xmlFileLinq.Descendants("name") select food;
+                foreach (XElement element in foods)
+                {
+                    this.output_box.Text += (element.Value + "\r\n");
+                }
+            }
+        }
+
+        private void output_box_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void setMaskUp()
+        {
+            this.maskUp =
+            @"<xsd:schema xmlns:xsd='http://www.w3.org/2001/XMLSchema'>  
+               <xsd:element name='breakfast_menu'>  
+                <xsd:complexType>  
+                 <xsd:sequence>
+                   <xsd:element name='food'>  
+                    <xsd:complexType>  
+                     <xsd:sequence>
+                       <xsd:element name='name' type='xsd:string'/>  
+                       <xsd:element name='price' type='xsd:decimal'/>  
+                       <xsd:element name='description' type='xsd:string'/>  
+                       <xsd:element name='calories' type='xsd:integer'/>  
+                     </xsd:sequence>  
+                    </xsd:complexType>  
+                  </xsd:element>  
+                 </xsd:sequence>  
+                </xsd:complexType>  
+               </xsd:element>  
+              </xsd:schema>";
+        }
+
+        private void setSchema()
+        {
+            this.schemas = new XmlSchemaSet();
+            // using System.IO for StringReader;
+            this.schemas.Add("", XmlReader.Create(new StringReader(this.maskUp)));
+        }
+
+        private void only_name_Click(object sender, EventArgs e)
+        {
+            this.output_box.Text = "";
+            IEnumerable<XElement> foods = from food in this.xmlFileLinq.Descendants("name") select food;
+            foreach (XElement element in foods)
+            {
+                this.output_box.Text += (element.Value + "\r\n");
+            }
+            this.mode = 2;
+        }
+
+        private void allSchema_Click(object sender, EventArgs e)
+        {
+           this.output_box.Text = "";
+           this.output_box.Text += this.xmlFileLinq;
+           this.mode = 1;
         }
     }
 }
